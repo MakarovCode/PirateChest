76912a0542429308b3d4a8993585fb9dcf42e8dd
Simon C <simoncorreaocampo@gmail.com>
2021-06-05 20:01:16 -0500

    Cert fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
       if Rails.env == "development"
         system "cd #{Rails.root.join('certificates/users')} && mkdir #{self.id} && cd #{self.id} && ssh-keygen -t rsa -b 2048 -v -f private_key.pem -N #{self.email} && openssl rsa -in private_key.pem -pubout -out public_key.pem -passin pass:#{self.email}"
       else
-        system "cd /home/rails/piratechest/shared/certificates/users && mkdir #{self.id} && cd #{self.id} && ssh-keygen -t rsa -b 2048 -v -f private_key.pem -N #{self.email} && openssl rsa -in private_key.pem -pubout -out public_key.pem -passin pass:#{self.email}"
+        system "cd /home/rails/piratechest/shared/certificates/users && mkdir #{self.id} && cd #{self.id} && ssh-keygen -t rsa -b 2048 -v -f private_key.pem -N #{self.email} && ssh-keygen -y -f private_key.pem > public_key.pem -P #{self.email}"
       end
     end
 
